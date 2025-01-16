0fb4e583a7b4a9366f1523ca997b46a486a48cd7
Roshan Varughese <40583749+Animeboynz@users.noreply.github.com>
2025-01-17 11:11:31 +1300

    Updating Logo

--- v0.2/KMD/app/build.gradle.kts
+++ v0.2/KMD/app/build.gradle.kts
@@ -19,10 +19,10 @@ android {
         applicationId = "com.animeboynz.kmd"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        versionCode = 1
+        versionName = "0.1.0"
     }
 
     buildTypes {
