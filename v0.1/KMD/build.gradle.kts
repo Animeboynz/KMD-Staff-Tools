033bea4a9a5a7b2d45c719e800f27696c1da2094
Roshan Varughese <40583749+Animeboynz@users.noreply.github.com>
2025-01-25 18:56:21 +1300

    Delete Old Version

--- v0.1/KMD/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id("app.cash.sqldelight") version "2.0.1" apply false
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    kotlin("jvm") version "1.9.25"
-}
-// Turn it off and on again
