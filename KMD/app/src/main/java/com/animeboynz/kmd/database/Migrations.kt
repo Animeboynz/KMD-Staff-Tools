47739578e9f33fb1e5ac5bdb684bbe413d595716
Roshan Varughese <40583749+Animeboynz@users.noreply.github.com>
2025-02-06 00:53:44 +1300

    Demo Migrations

--- KMD/app/src/main/java/com/animeboynz/kmd/database/Migrations.kt
+++ KMD/app/src/main/java/com/animeboynz/kmd/database/Migrations.kt
@@ -1,5 +1,22 @@
 package com.animeboynz.kmd.database
 
 import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-val Migrations: Array<Migration> = arrayOf()
+val migration_1_2 = object : Migration(1, 2) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("ALTER TABLE ProductsEntity ADD COLUMN category TEXT NOT NULL DEFAULT ''")
+    }
+}
+
+val migration_2_3 = object : Migration(2, 3) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("ALTER TABLE ProductsEntity ADD COLUMN category TEXT NOT NULL DEFAULT ''")
+    }
+}
+
+
+val Migrations: Array<Migration> = arrayOf(
+    //migration_1_2,
+    //migration_2_3,
+)
